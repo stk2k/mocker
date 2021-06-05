342db80d97b6a21d4b7710ec0708815dea232315
Katsuki Shuto <stk2k@sazysoft.com>
2021-06-05 18:38:38 +0900

    Signed-off-by: Katsuki Shuto <stk2k@sazysoft.com>

--- test/CodeGeneratorTest.php
+++ test/CodeGeneratorTest.php
@@ -22,6 +22,7 @@ final class CodeGeneratorTest extends TestCase
             CodeGenerator::generateClassCode(AbstractBazz::class, $save_dir, new DefaultMockedClassNameProvider());
         }
         catch(Exception $ex){
+            echo $ex->getTraceAsString();
             $this->fail($ex->getMessage());
         }
 
